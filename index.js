5bf59eb40e4395fc400330ed57fd9e9c88369d54
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-04-18 15:35:57 -0700

    add prompts for employees, roles, departments

--- index.js
+++ index.js
@@ -88,3 +88,166 @@ function dept() {
     }
   });
 }
+// employees menu
+function employees() {
+  inquirer.prompt([
+    {
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: ["View Employees",
+        "Add an Employee",
+        "Delete an Employee",
+        "Update Employee Role",
+        "Update Manager",
+        "View Employees by Manager",
+        "Return to Start Menu"]
+    }])
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View Employees":
+          viewEmployees();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Delete Employee":
+          delEmployee();
+          break;
+
+        case "Update Employee Salary":
+          updateEmployeeSalary();
+          break;
+
+        case "Update Manager":
+          updateManager();
+          break;
+
+        case "View Employees by Manager":
+          managerEmployees();
+          break;
+
+        case "Return to Main Menu":
+          start();
+          break;
+      }
+    });
+}
+// build end function to end session in app
+function finish() {
+  console.log("Thank you and be safe out there!")
+  connection.end();
+}
+// build view department, selecting all from dept. table
+// display using console.table dependency
+async function viewDepartments() {
+  var query = "SELECT name as Department, dept_id as 'Dept ID' FROM department"
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.log("\n")
+    console.table(res);
+    setTimeout(start, 1000)
+  })
+};
+// build view role, selecting all from role table
+// display using console.table dependency
+async function viewRoles() {
+  var query = "SELECT title as Title, role_id as 'Role #', salary as Salary, department.name as Department FROM role LEFT JOIN department ON role.dept_id = department.dept_id"
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.log("\n")
+    console.table(res);
+    setTimeout(start, 1000)
+  })
+
+};
+// build view employee, selecting all from employee table
+// display using console.table dependency
+async function viewEmployees() {
+  var query = "SELECT emp_id as 'ID #', first_name as First, last_name as Last, department.name as Department, role.title as Title, role.salary as Salary FROM employee LEFT JOIN role on employee.role_id = role.role_id LEFT JOIN department on role.dept_id = department.dept_id"
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.log("\n")
+    console.table(res);
+    setTimeout(start, 1000)
+  })
+};
+// adding department
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "dept",
+        type: "input",
+        message: "What department do you want to add?"
+      }])
+    .then(function (answer) {
+      console.log(answer.dept)
+      var query = `INSERT INTO department (name) VALUES ("${answer.dept}")`;
+      connection.query(query, answer.dept, function (err, res) {
+        if (err) throw err
+        viewDepartments();
+      })
+    });
+}
+//adding roles
+function addRole() {
+  inquirer.prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "What role do you want to add?"
+    },
+    {
+      name: "salary",
+      type: "input",
+      message: "Please indicate the salary for that role."
+    },
+    {
+      name: "dept",
+      type: "input",
+      message: "What is the department id for this role?:"
+    }])
+    .then(function (answer) {
+      console.log(answer)
+      var query = `INSERT INTO role (title,salary,dept_id) VALUES ("${answer.title}",${answer.salary},${answer.dept})`;
+      connection.query(query, answer, function (err, res) {
+        if (err) throw err
+        viewRoles();
+      })
+    });
+  }
+  //adding employee
+  function addEmployee() {
+    inquirer.prompt([
+      {
+        name: "first",
+        type: "input",
+        message: "Enter the first name."
+      },
+      {
+        name: "last",
+        type: "input",
+        message: "Enter the last name."
+      },
+      {
+        name: "role",
+        type: "input",
+        message: "What is the role id?"
+      },
+      {
+        name: "manager",
+        type: "input",
+        message: "What is the manager id?",
+        default: 1
+      }])
+      .then(function (answer) {
+        var query = `INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ("${answer.first}","${answer.last}",${answer.role},${answer.manager})`;
+        connection.query(query, answer, function (err, res) {
+          if (err) throw err
+          viewEmployees();
+        });
+      });
+  }
